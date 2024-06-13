e84084dd89da15d60d0c436963efb46e0e7c5199
Huong Giang <ngothihuonggiang58@gmail.com>
2024-06-13 14:31:00 +0700

    sửa lại comment theo chuẩn jsdoc

--- src/helpers/incrementString.tsx
+++ src/helpers/incrementString.tsx
@@ -1,19 +1,22 @@
 /**
- * Chuyển đối số đầu vào thành chuỗi.
- * Lấy phần số từ chuỗi. Nếu không tìm thấy số, gán giá trị "0".
- * Lưu độ dài của phần số.
- * Tăng phần số lên 1.
- * Nếu có số 0 đằng trước, thêm chúng lại để duy trì độ dài ban đầu.
- * Thay thế phần số cũ trong chuỗi ban đầu bằng phần số mới đã được tăng giá trị.
- * @param {string} string Chuỗi đầu vào chứa chuỗi số cần tăng.
- * @returns {string} Chuỗi mới sau khi đã tăng giá trị chuỗi số.
+ * Increments the numeric part of a string by 1.
+ * @param {string} string - The input string containing the numeric part to increment.
+ * @returns {string} The new string with the incremented numeric part.
+ * 
+ * The function performs the following steps:
+ * 1. Converts the input to a string (in case it isn't already).
+ * 2. Extracts the numeric part of the string. If no numeric part is found, defaults to "0".
+ * 3. Stores the length of the numeric part.
+ * 4. Increments the numeric part by 1.
+ * 5. Pads the incremented numeric part with leading zeros to maintain its original length.
+ * 6. Replaces the old numeric part in the original string with the new incremented numeric part.
  */
-const incrementString = (string: string): string =>{
+const incrementString = (string: string): string => {
   const str = string.toString();
   let number = str.match(/\d+/) === null ? "0" : str.match(/\d+/)?.[0] || "";
   const numberLength = number.length;
   number = (parseInt(number) + 1).toString();
-  while(number.length < numberLength){
+  while (number.length < numberLength) {
     number = "0" + number;
   }
   return str.replace(/\d+/g, "").concat(number);
